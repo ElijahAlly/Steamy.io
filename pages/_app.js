5e1ec491ffbedc06290cc2c46543ca5cb5f37ecb
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2024-08-15 16:47:24 -0400

    updated provider location

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '~/styles/style.scss'
 import React from 'react'
 import DefaultLayout from '~/components/DefaultLayout'
-import UserProvider from '~/providers/UserProvider'
+import UserProvider from '~/lib/UserProvider'
 
-export default function SupabaseSession({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <DefaultLayout>
